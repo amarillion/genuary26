970e12e9d6db69534f6608cf74d18b4a64704dfe
Martijn van Iersel <mvaniersel@gmail.com>
2026-01-02 19:11:02 +0100

    Experiment: FULL_ES3 instead of FULL_ES2

--- make-emscripten-buildenv.sh
+++ make-emscripten-buildenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE=amarillion/alleg5-emscripten:5.2.10.1
+IMAGE=amarillion/alleg5-emscripten:5.2.10.1-es3
 SRCDIR=$(dirname $(readlink -f $0))
 docker run \
 	-u $(id -u):$(id -g) \
